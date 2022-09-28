963b186187404d0e46443064bf0a439a0faf8fe0
Gustavo Gomes <gustavo.sgomess@gmail.com>
2022-09-28 18:26:16 -0300

    Configurando o endpoint h2-console para ser liberado pelo spring security

---
+++ src/main/java/com/projeto/aprendizado/infraestrutura/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.projeto.aprendizado.infraestrutura.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private Environment environment;
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/h2-console/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/produtos/**",
+            "/categorias/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/clientes/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.cors().and().csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+}
